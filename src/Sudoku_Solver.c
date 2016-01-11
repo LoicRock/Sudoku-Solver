bc66f604af2b0f5fb359b8bfbb400085df3dbc1c
LoicRock <loic.sculier@hotmail.fr>
2016-01-11 12:29:20 +0100

    Backtracking for 4x4 and 6x6 + display
    Backtracking work for 4x4 and 6x6 + display with separator

--- src/Sudoku_Solver.c
+++ src/Sudoku_Solver.c
@@ -10,24 +10,67 @@ typedef struct
     char** value;
     int nbr;
     int nbc;
+    int size_cell_c;
+    int size_cell_r;
 } Sudoku;
 
-void display_sudoku(int nbc,int nbr,char s[nbc][nbr])
+void display_separator(int nbc,int nbr)
+{
+
+    if(nbr==4)
+    {
+        printf("+------+------+\n");
+    }
+    else if(nbr==6)
+    {
+        printf("+---------+---------+\n");
+    }
+    else if(nbr==9)
+    {
+        printf("+---------+---------+---------+\n");
+    }
+    else if(nbr==16)
+    {
+        printf("+------+------+------+------+\n");
+    }
+
+}
+
+void display_sudoku(int nbc,int nbr,char s[nbc][nbr],int sizer,int sizec)
 {
     int i,j;
+
+    display_separator(nbc,nbr);
+
+
     for (i=0; i<nbc; i++)
     {
+        printf("|");
         for(j=0; j<nbr; j++)
         {
-            printf(" %c |",s[i][j]);
+
+            if(j==sizer || j==sizer*2)
+            {
+                printf("|");
+            }
+
+            printf(" %c ",s[i][j]);
             if(j==nbc-1)
             {
+                printf("|");
                 printf("\n");
+                if(i==sizer-1 || i==(sizer*2)-1 || i==(sizer*3)-1)
+                {
+                    display_separator(nbc,nbr);
 
+                }
             }
+
         }
     }
-    printf("OK");
+
+
+
 
 
 }
@@ -66,8 +109,6 @@ void lire_fichier(int nbc,int nbr,char s[nbc][nbr],char *filename)
     int i,j;
     FILE* fic=NULL;
     fic=fopen(filename,"r");
-    char c;
-    ;
 
     if(fic!=NULL)
     {
@@ -120,35 +161,194 @@ int countlines(char *filename)
     return lines;
 }
 
+
+void print_bool(bool a)
+{
+    if (a==true)
+    {
+
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+
+}
+
+//backtracking
+
+bool absentSurColonne(int nbr,int nbc, int k,char s[nbr][nbc],int j)
+{
+    char a;
+    int i;
+    for (i=0; i < nbc; i++)
+    {
+        a = k + '0';
+        if (s[i][j] == a)
+            return false;
+    }
+    return true;
+}
+
+bool absentSurLigne(int nbr,int nbc, int k,char s[nbr][nbc],int i)
+{
+    char a;
+    int j ;
+    for (j=0; j < nbr; j++)
+    {
+        a = k + '0';
+        if (s[i][j] == a)
+            return false;
+    }
+    return true;
+}
+
+bool absentSurBloc (int nbr,int nbc,char s[nbc][nbr],int k, int i, int j,int sizer,int sizec)
+{
+    char a;
+
+    int i2 = i-(i%sizer);
+    int j2 = j-(j%sizec);  // ou encore : _i = 3*(i/3), _j = 3*(j/3);
+    for (i=i2; i < i2+sizer; i++)
+        for (j=j2; j < j2+sizec; j++)
+            a = k + '0';
+    if (s[i][j] == a)
+        return false;
+    return true;
+}
+
+
+bool estValide (int nbc,int nbr,char s[nbc][nbr], int position,int sizec,int sizer)
+{
+
+    char a;
+    if (position == nbc*nbr)
+        return true;
+
+    int i = position/nbc;
+    int j = position%nbc;
+
+    if (s[i][j] != ' ')
+    {
+        return estValide(nbc,nbr,s,position+1,sizec,sizer);
+    }
+
+
+    int k;
+    for (k=1; k<=nbc; k++)
+    {
+        printf("k: %d \n",k);
+        print_bool(absentSurColonne(nbr,nbc,k,s,j));
+        print_bool(absentSurLigne(nbr,nbc,k,s,i ) );
+        print_bool(absentSurBloc(nbr,nbc,s,k,i,j,sizec,sizer));
+        printf("\n");
+
+        if (absentSurColonne(nbr,nbc,k,s,j)==true && absentSurLigne(nbr,nbc,k,s,i )==true && absentSurBloc(nbr,nbc,s,k,i,j,sizec,sizer)==true)
+        {
+            a = k+'0';
+            s[i][j] = a;
+
+            if (estValide (nbc,nbr,s, position+1,sizec,sizer))
+                return true;
+        }
+    }
+    s[i][j] = ' ';
+
+    return false;
+}
+
+int select_size_cell_r(int nbline)
+{
+    int size;
+
+    switch(nbline)
+    {
+    case 4:
+        size=2;
+
+        break;
+    case 6:
+        size=2;
+        break;
+    case 9:
+        size=3;
+        break;
+    case 16:
+        size=4;
+        ;
+        break;
+    default:
+        size=0;
+    }
+
+    return size;
+}
+
+int select_size_cell_c(int nbline)
+{
+
+    int size;
+
+    switch(nbline)
+    {
+    case 4:
+        size=2;
+
+        break;
+    case 6:
+        size=3;
+        break;
+    case 9:
+        size=3;
+        break;
+    case 16:
+        size=4;
+        ;
+        break;
+    default:
+        size=0;
+    }
+
+    return size;
+}
+
+
 int main()
 {
     Sudoku s;
 
     char fnamer[100]="";
-    printf("Entrez le nom du fichier [exemple.txt]: \n");
+    printf("Enter a name file [exemple.txt]: \n");
     scanf("%s",&fnamer);
     int nbline=countlines(fnamer);
-    printf("%d",nbline);
+
+
 
     //init taille grille
     s.nbc=nbline;
     s.nbr=nbline;
     char tab[s.nbr][s.nbc];
     s.value=tab;
+    int sizec,sizer;
+    s.size_cell_c=select_size_cell_c(nbline);
+    s.size_cell_r=select_size_cell_r(nbline);
+    printf("%d",s.size_cell_c);
+    printf("%d",s.size_cell_r);
 
 
-    init_sudoku(s.nbc,s.nbr,s.value);//Initialise la grille Ã  0
-    display_sudoku(s.nbc,s.nbr,s.value);
+
+    init_sudoku(s.nbc,s.nbr,s.value);//Initialise la grille à 0
     lire_fichier(s.nbc,s.nbr,s.value,fnamer); //Lit le fichier et remplis la grille
-    printf("AprÃ©s lecture \n");
-    display_sudoku(s.nbc,s.nbr,s.value);
-    printf("AprÃ©s remplacement :\n");
+    printf("Aprés lecture du fichier \n");
+    display_sudoku(s.nbc,s.nbr,s.value,s.size_cell_c,s.size_cell_r);
+    printf("Aprés remplacement des zero:\n");
     zero_to_space(s.nbc,s.nbr,s.value); //Remplace les zero par des espace
-    display_sudoku(s.nbc,s.nbr,s.value);
+    display_sudoku(s.nbc,s.nbr,s.value,s.size_cell_c,s.size_cell_r);
 
-    printf("RÃ©solution : \n");
-    estValide(s.nbc,s.nbr,s.value,0);
-     display_sudoku(s.nbc,s.nbr,s.value);
+    printf("Résolution : \n");
+    estValide(s.nbc,s.nbr,s.value,0,s.size_cell_c,s.size_cell_r);
+    display_sudoku(s.nbc,s.nbr,s.value,s.size_cell_c,s.size_cell_r);
 
     return 0;
 }
